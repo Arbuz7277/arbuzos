585030bb1864eaf78b688246e481362c776c7a6f
arbuz <gosha.telegrambot@gmail.com>
2026-09-25 20:05:59 +0000

    feat(GDT): add code and data selectors for ring3

--- protected_mode/gdt.asm
+++ protected_mode/gdt.asm
@@ -3,21 +3,37 @@
 gdt_start:
     dq 0
 
-gdt_code:
+gdt_code0:
     dw 0xFFFF        ; limit 0:15
     dw 0x0000        ; base 0:15
     db 0x00          ; base 16:23
-    db 0x9A          ; access
-    db 0xCF          ; flags, limit 16:19
+    db 10011010b     ; access
+    db 11001111b     ; flags, limit 16:19
     db 0x00          ; base 24:31
 
-gdt_data:
-    dw 0xFFFF        ; limit 0:15
-    dw 0x0000        ; base 0:15
-    db 0x00          ; base 16:23
-    db 0x92          ; access
-    db 0xCF          ; flags, limit 16:19
-    db 0x00          ; base 24:31
+gdt_data0:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_code3:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 11111010b
+    db 11001111b
+    db 0x00
+
+gdt_data3:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 11110010b
+    db 11001111b
+    db 0x00
 
 gdt_end:
 
